23802e12ba516cc16a515d4ed6d7c1e521918a94
Ivan <ivanlinux@gmail.com>
2015-03-11 00:02:33 +0000

    output

--- TestCalc.py
+++ TestCalc.py
@@ -7,7 +7,16 @@ class TestCal (unittest.TestCase):
 		self.calc = Calc ()
 
 	def test_00_sum (self):
+		print "Hello world"
 		self.assertEqual(self.calc.sum(2,2),4)
+
+	def test_01_sum2 (self):
+		print "This test should fail"
+		self.assertEqual(self.calc.sum(2,3),6)
+
+	def test_02_sub (self):
+		print "2 minus 2 = %d" self.calc.sub(2,2)
+		self.assertEqual(self.calc.sub(2,2),0)
 		
 if __name__ == '__main__':
 	#suite = unittest.TestLoader().loadTestsFromTestCase(TestCal)
