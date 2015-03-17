8693503ee7804a8b9045578584ce1ae35a86cc41
Ivan Rodriguez <ivan_rodriguez@xyratex.com>
2015-03-17 10:04:34 +0000

    Testing improvements

--- TestCalc.py
+++ TestCalc.py
@@ -1,13 +1,25 @@
 from Calc import Calc
-import unittest
+from xmlrunner.unittest import unittest
+import xmlrunner
 
 class TestCal (unittest.TestCase):
 	def setUp(self):
 		self.calc = Calc ()
 
 	def test_00_sum (self):
+		print "Hello world"
 		self.assertEqual(self.calc.sum(2,2),4)
+
+	def test_01_sum2 (self):
+		print "This test should not fail"
+		self.assertEqual(self.calc.sum(2,3),5)
+
+	def test_02_sub (self):
+		print "2 minus 2 = %d" %self.calc.sub(2,2)
+		self.assertEqual(self.calc.sub(2,2),0)
 		
 if __name__ == '__main__':
-	suite = unittest.TestLoader().loadTestsFromTestCase(TestCal)
-	unittest.TextTestRunner(verbosity=2).run(suite)
+	#suite = unittest.TestLoader().loadTestsFromTestCase(TestCal)
+	#unittest.TextTestRunner(verbosity=2).run(suite)
+
+	unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports', verbosity=3))
